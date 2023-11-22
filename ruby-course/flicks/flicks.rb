0165be0a72dbb38e7c0bb09904d9aeb3a1f96eb7
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-11-22 14:38:37 +1100

    coompleted lesson 3 - objects

--- ruby-course/flicks/flicks.rb
+++ ruby-course/flicks/flicks.rb
@@ -10,3 +10,15 @@ puts "#{title} has a rank of #{rank}."
 puts "Mikey's favourite movie is #{title}.\nHe gives it a #{rank}!"
 
 # single quoted strings cannot be interpolated
+
+#methods ending in ! are called dangerous methods
+#methods ending in ? are called predicate methods,
+# they return true or false
+puts title.center(10)
+puts title.ljust(10, '.')
+puts title.upcase
+puts title
+puts title.upcase!
+puts title
+puts title.capitalize!
+puts title
--- ruby-course/studio_game/studio_game.rb
+++ ruby-course/studio_game/studio_game.rb
@@ -1,4 +1,14 @@
 puts "Let's play a game!\n#{'🚀'*25}\n3\n2\n1"
-player_name = "Finn"
+player_name = "finn"
 player_health = 60
 puts "#{player_name}'s health is #{player_health*3/9.0}."
+puts "#{player_name} has a health of #{player_health}."
+puts "#{player_name.upcase} has a health of #{player_health}."
+puts "#{player_name} has a health of #{player_health}.".center(50, "*")
+puts "#{player_name.ljust(30, ".")} #{player_health} health"
+puts "#{player_name.capitalize} has a health of #{player_health} health"
+puts "#{player_name.strip.capitalize} has a health of #{player_health} health"
+
+puts player_health.to_s.reverse
+puts player_health.to_s.reverse.to_i
+puts player_health.pow(2)
